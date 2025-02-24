5106e547cbd198f68df7582ceb663dba4047d981
PageCreativeStudio <web@pagecreative.co.uk>
2025-02-24 17:14:39 +0000

    Updated wp-admin

---
+++ wp-content/themes/pagecreative/footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package pagecreative
+ */
+
+?>
+
+<footer id="colophon" class="site-footer">
+
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+
+<!-- Owl Carousel CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<!-- Files Path -->
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/common.js"></script>
+<?php wp_footer(); ?>
+</main>
+</body>
+
+</html>
