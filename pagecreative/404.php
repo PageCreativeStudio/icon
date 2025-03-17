fede3bdd93a3d6fe10835cd4dd877592e7aa6969
PageCreativeStudio <web@pagecreative.co.uk>
2025-03-17 10:44:45 +0000

    Updated

--- pagecreative/404.php
+++ pagecreative/404.php
@@ -14,12 +14,13 @@ get_header();
 
 	<section class="error-404 not-found text-center ">
 		<header class="page-header">
-			<h1 class="font-50 font-mb-25">Oops! That page can't be found. </h1>
+			<h1 class="font-50 font-mb-25 px-2">Oops! That page can't be found. </h1>
 		</header>
 
-		<div class="page-content text-center">
-			<p class="font-18 font-300 max-30 mx-auto">It looks like nothing was found at this location. Maybe try one
+		<div class="page-content text-center max-30 mx-auto px-3">
+			<p class="font-18 font-300">It looks like nothing was found at this location. Maybe try one
 				of the links below or a search?</p>
+			<a class="uppercase" href="<?php echo home_url(); ?>">back to home</a>
 		</div>
 	</section>
 
