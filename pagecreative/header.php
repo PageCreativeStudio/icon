9d997b437625aa61ebc3cffcd0b3375092fa10b3
PageCreativeStudio <web@pagecreative.co.uk>
2025-03-20 10:29:59 +0000

    Updated

--- pagecreative/header.php
+++ pagecreative/header.php
@@ -30,7 +30,7 @@
 	</script>
 
 	<!-- Google tag manager (gtag.js) -->
-	<script>(function (w, d, s, l, i) {
+	<!--<script>(function (w, d, s, l, i) {
 			w[l] = w[l] || []; w[l].push({
 				'gtm.start':
 					new Date().getTime(), event: 'gtm.js'
@@ -38,7 +38,7 @@
 				j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
 					'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
 		})(window, document, 'script', 'dataLayer', 'GTM-K9NTKGT');
-	</script>
+	</script>-->
 
 	<?php wp_head(); ?>
 </head>
