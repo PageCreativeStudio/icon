9e613331ac3c5399552c82c0fc0d5656cc1ec719
PageCreativeStudio <web@pagecreative.co.uk>
2025-03-20 10:28:11 +0000

    Updated

--- pagecreative/template-parts/blocks/hero-section.php
+++ pagecreative/template-parts/blocks/hero-section.php
@@ -1,4 +1,4 @@
-<section class="hero d-flex flex-wrap justify-content-between">
+<section class="hero d-flex flex-wrap justify-content-between align-items-center">
     <h1><?php echo get_field('hero_title'); ?></h1>
     <p><?php echo get_field('hero_description'); ?></p>
     <img src="<?php echo get_field('hero_image'); ?>" alt="">
