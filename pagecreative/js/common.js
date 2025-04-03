d1d39ddd96090483c34c8fb17810342b607f0194
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-03 10:11:38 +0100

    Updated -v1.1

--- pagecreative/js/common.js
+++ pagecreative/js/common.js
@@ -148,7 +148,7 @@ jQuery(document).ready(function ($) {
                 stagePadding:40,
                 center: true
             },
-            1700: {
+            1770: {
                 items: 5,
                 stagePadding:80,
                 center: true
