58539c995069a05ea75fcd0ac30b0ff871f2f213
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-03 17:01:44 +0100

    Updated -v1.1

--- pagecreative/template-parts/blocks/testimonials.php
+++ pagecreative/template-parts/blocks/testimonials.php
@@ -5,7 +5,7 @@
                 <?php while (have_rows('testimonial_repeater', 'options')) {
                     the_row(); ?>
                     <div class="review-item">
-                        <p class="text-dark font-22 font-mb-18 px-2 max-40 mx-auto">
+                        <p class="text-dark font-22 font-mb-18 px-2 max-50 mx-auto">
                             <?php echo get_sub_field('text', 'options'); ?></p>
                         <p class="font-15 text-dark"><?php echo get_sub_field('author', 'options'); ?></p>
                     </div>
