60c69f5c8ee94825c0d781e0e1ce49915e9d7542
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-03 15:12:27 +0100

    Updated -v1.1

--- pagecreative/header.php
+++ pagecreative/header.php
@@ -55,7 +55,8 @@
 			<header id="masthead" class="site-header mx-auto text-center pt-2 pt-lg-3 pb-2 pb-lg-4 px-md-4">
 				<div class="container-fluid mx-auto">
 					<div class="row py-3 py-lg-0">
-						<nav class="main-navigation d-flex justify-content-end justify-content-lg-start place-items-center col-6 col-lg-5 order-2 order-lg-1 px-0 px-lg">
+						<nav
+							class="main-navigation d-flex justify-content-end justify-content-lg-start place-items-center col-6 col-lg-5 order-2 order-lg-1 px-0 px-lg">
 							<button class="menu-toggle float-right text-white" id="toggle-menu" aria-label="menu">
 								<svg xmlns="http://www.w3.org/2000/svg" width="13" height="9" viewBox="0 0 13 9"
 									fill="none">
@@ -72,7 +73,8 @@
 							);
 							?>
 						</nav>
-						<div class="site-branding order-1 order-lg-2 col-6 col-lg-2 pl-lg-1 pr-lg-4 text-left text-lg-center align-self-center">
+						<div
+							class="site-branding order-1 order-lg-2 col-6 col-lg-2 pl-lg-1 pr-lg-4 text-left text-lg-center align-self-center">
 							<a href="<?php echo home_url(); ?>">
 								<img class="w-100 max-6"
 									src="<?php echo esc_url(get_template_directory_uri() . '/include/images/svg-logo.svg'); ?>"
@@ -106,93 +108,105 @@
 						</div>
 					</div>
 
-					<div class="mobmenu container w-100 py-2">
-						<div class="row px-3 pt-2">
-							<div class="col-9 mob_logo align-self-center px-0">
-								<div class="d-flex flex-wrap">
-									<a class="cartbtn" href="<?php echo home_url(); ?>/cart">
-										<svg xmlns="http://www.w3.org/2000/svg" width="26" height="25"
-											viewBox="0 0 26 25" fill="none">
-											<g clip-path="url(#clip0_491_131)">
-												<path fill-rule="evenodd" clip-rule="evenodd"
-													d="M17.156 2.04199C17.156 4.35267 15.2654 6.24322 12.9547 6.24322C10.6441 6.24322 8.75351 4.35267 8.75351 2.04199H5.60259L1.40137 4.14261V10.4444H5.60259V23.0481H20.3069V10.4444H24.5081V4.14261L20.3069 2.04199H17.156Z"
-													stroke="#343434" stroke-width="2.10061" stroke-linecap="square"
-													stroke-linejoin="round" />
-												<rect x="14.3755" y="9.40039" width="3.27222" height="3.39852" rx="0.5"
-													fill="#343434" />
-											</g>
-											<defs>
-												<clipPath id="clip0_491_131">
-													<rect width="25.2074" height="24.1571" fill="white"
-														transform="translate(0.351074 0.466797)" />
-												</clipPath>
-											</defs>
-										</svg>
-									</a>
-									<div class="align-self-center pl-4">
-									    <img class="w-100 max-7"
-										src="<?php echo esc_url(get_template_directory_uri() . '/include/images//svg-logo.svg'); ?>"
-										alt="icon printing">
+					<div class="mobmenu container w-100 p-0">
+						<div class="h-100 bg-white py-2 px-2 mobmenu__container">
+							<div class="row px-3 pt-2 mx-0">
+								<div class="col-9 mob_logo align-self-center px-0 close-on-sliding-active">
+									<div class="d-flex flex-wrap">
+										<a class="cartbtn" href="<?php echo home_url(); ?>/cart">
+											<svg xmlns="http://www.w3.org/2000/svg" width="26" height="25"
+												viewBox="0 0 26 25" fill="none">
+												<g clip-path="url(#clip0_491_131)">
+													<path fill-rule="evenodd" clip-rule="evenodd"
+														d="M17.156 2.04199C17.156 4.35267 15.2654 6.24322 12.9547 6.24322C10.6441 6.24322 8.75351 4.35267 8.75351 2.04199H5.60259L1.40137 4.14261V10.4444H5.60259V23.0481H20.3069V10.4444H24.5081V4.14261L20.3069 2.04199H17.156Z"
+														stroke="#343434" stroke-width="2.10061" stroke-linecap="square"
+														stroke-linejoin="round" />
+													<rect x="14.3755" y="9.40039" width="3.27222" height="3.39852"
+														rx="0.5" fill="#343434" />
+												</g>
+												<defs>
+													<clipPath id="clip0_491_131">
+														<rect width="25.2074" height="24.1571" fill="white"
+															transform="translate(0.351074 0.466797)" />
+													</clipPath>
+												</defs>
+											</svg>
+										</a>
+										<div class="align-self-center pl-4">
+											<img class="w-100 max-7"
+												src="<?php echo esc_url(get_template_directory_uri() . '/include/images//svg-logo.svg'); ?>"
+												alt="icon printing">
+										</div>
 									</div>
 								</div>
-							</div>
-							<div class="col-3 align-self-center text-right px-0">
-								<a class="closebtn text-white font-400">
-									<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"
-										fill="none">
-										<path d="M1.54053 13.5L13.7498 1.5M1.54053 1.5L13.7498 13.5" stroke="#333333"
-											stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-									</svg>
-								</a>
-							</div>
-							<div class="mobmain_menu col-12 py-4 px-0">
-								<?php
-								wp_nav_menu(
-									array(
-										'theme_location' => 'menu-1',
-										'menu_id' => 'primary-menu',
-									)
-								);
-								?>
-							</div>
+								<div class="col-3 align-self-center text-right px-0 close-on-sliding-active">
+									<a class="closebtn text-white font-400">
+										<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
+											viewBox="0 0 15 15" fill="none">
+											<path d="M1.54053 13.5L13.7498 1.5M1.54053 1.5L13.7498 13.5"
+												stroke="#333333" stroke-width="1.5" stroke-linecap="round"
+												stroke-linejoin="round" />
+										</svg>
+									</a>
+								</div>
+								<div class="mobmain_menu col-12 py-4 px-0">
+									<?php
+									wp_nav_menu(
+										array(
+											'theme_location' => 'menu-1',
+											'menu_id' => 'primary-menu',
+										)
+									);
+									?>
+								</div>
 
-							<div class="sm__header d-flex px-2 py-4">
-								<a class="mr-2" href="" target="_blank">
+								<div class="sm__header d-flex px-2 py-4">
+									<a class="mr-2" href="" target="_blank">
 
-								</a>
-								<a href="/" target="_blank">
+									</a>
+									<a href="/" target="_blank">
 
-								</a>
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				
+
 				<section class="bg-white pb-5 pt-0 megamenu__container" id="services__mega-menu">
 					<div class="container-fluid px-3">
 						<div class="row justify-content-between">
-						     <?php if (have_rows('mega_menu', 'options')) { ?>
-                                <?php while (have_rows('mega_menu', 'options')) { the_row(); ?>
-                                    <div class="col-12 col-md-3 col-lg-3 col-xl-2 mt-5 text-left px-3">
-        								<div class="mx-3 megamenu__col ">
-        								    <h2 class="text-black font-bold font-18 mb-1 mt-2 pb-3"><?php echo get_sub_field('sub_menu_title', 'options'); ?></h2>
-            								<ul class="m-0 p-0 list-unstyled">
-            								    <?php if (have_rows('list_repeater', 'options')) { ?>
-                                                    <?php while (have_rows('list_repeater', 'options')) { the_row(); ?>
-                                                        <li class="pb-1"><a class="text-gray font-14 mb-0" href="<?php echo get_sub_field('list_link', 'options'); ?>"><?php echo get_sub_field('list_title', 'options'); ?></a></li>
-                                                    <?php } ?>
-                                                <?php } ?>  
-            									
-            								</ul>
-        								</div>
-        							</div>
-                                <?php } ?>
-                            <?php } ?>  
-							<div class="col-12 col-md-3 col-lg-3 col-xl-2 promo__col mt-5 text-left px-4 py-5" style="max-height:22rem; background: linear-gradient(0deg, rgba(51, 51, 51, 0.70) 0%, rgba(51, 51, 51, 0.70) 100%), url(<?php echo get_field('promo_image', 'options'); ?>) lightgray -104.911px 0px / 254.545% 100% no-repeat;background-size: cover; background-position-x: left;">
-							    <a class="h-100 d-block" href="#">
-							        <img class="d-block" src="<?php echo esc_url(get_template_directory_uri() . '/include/images/logo-white.svg'); ?>" alt="icon printing white logo">
-							        <span class="text-white arrow-white font-15 d-inline-block mt-3">View all products</span>
-							    </a>
+							<?php if (have_rows('mega_menu', 'options')) { ?>
+								<?php while (have_rows('mega_menu', 'options')) {
+									the_row(); ?>
+									<div class="col-12 col-md-3 col-lg-3 col-xl-2 mt-5 text-left px-3">
+										<div class="mx-3 megamenu__col ">
+											<h2 class="text-black font-bold font-18 mb-1 mt-2 pb-3">
+												<?php echo get_sub_field('sub_menu_title', 'options'); ?></h2>
+											<ul class="m-0 p-0 list-unstyled">
+												<?php if (have_rows('list_repeater', 'options')) { ?>
+													<?php while (have_rows('list_repeater', 'options')) {
+														the_row(); ?>
+														<li class="pb-1"><a class="text-gray font-14 mb-0"
+																href="<?php echo get_sub_field('list_link', 'options'); ?>"><?php echo get_sub_field('list_title', 'options'); ?></a>
+														</li>
+													<?php } ?>
+												<?php } ?>
+
+											</ul>
+										</div>
+									</div>
+								<?php } ?>
+							<?php } ?>
+							<div class="col-12 col-md-3 col-lg-3 col-xl-2 promo__col mt-5 text-left px-4 py-5"
+								style="max-height:22rem; background: linear-gradient(0deg, rgba(51, 51, 51, 0.70) 0%, rgba(51, 51, 51, 0.70) 100%), url(<?php echo get_field('promo_image', 'options'); ?>) lightgray -104.911px 0px / 254.545% 100% no-repeat;background-size: cover; background-position-x: left;">
+								<a class="h-100 d-block" href="#">
+									<img class="d-block"
+										src="<?php echo esc_url(get_template_directory_uri() . '/include/images/logo-white.svg'); ?>"
+										alt="icon printing white logo">
+									<span class="text-white arrow-white font-15 d-inline-block mt-3">View all
+										products</span>
+								</a>
 							</div>
 						</div>
 					</div>
