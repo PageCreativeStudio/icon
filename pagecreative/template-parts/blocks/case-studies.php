9c2ada7d9a410fab25cc074ad6a43e7e25391625
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-03 10:29:06 +0100

    Updated -v1.1

--- pagecreative/template-parts/blocks/case-studies.php
+++ pagecreative/template-parts/blocks/case-studies.php
@@ -1,6 +1,6 @@
 <section class="pb-5">
     <div class="container-fluid px-0 mx-auto mx-0 text-center">
-        <h2 class="font-18 text-dark mb-0 pb-2"><?php echo get_field('case_studies_title'); ?></h2>
+        <h2 class="font-20 font-mb-18 text-black mb-0 pb-2"><?php echo get_field('case_studies_title'); ?></h2>
         <a href="<?php echo home_url(); ?>/case-studies/" class="text-dark d-inline-block font-14 arrowblack mb-4 pb-2"><?php echo get_field('case_studies_sub_title'); ?></a>
         <div class="casestudies__slider owl-carousel owl-theme">
             <?php
