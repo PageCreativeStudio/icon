4ca0cd91ab0e5095f979517495505a4d597f8f9c
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-08 11:03:07 +0100

    Updated -v1.1

--- pagecreative/woocommerce/content-single-product.php
+++ pagecreative/woocommerce/content-single-product.php
@@ -13,41 +13,40 @@ global $product;
     <div class="container-fluid mx-auto px-md-4">
         <div class="row">
             <div class="col-12 col-lg-6">
-                <div class="product-images-container">
-                    <div class="featured-image">
-                        <!-- Featured Image -->
-                        <?php if (has_post_thumbnail()): ?>
-                            <div class="main-image">
-                                <?php the_post_thumbnail('full'); // Display the main featured image ?>
-                            </div>
-                        <?php else: ?>
-                            <img src="path-to-default-image.jpg" alt="No gallery image" class="default-featured-image">
-                            <!-- Fallback image if no gallery -->
-                        <?php endif; ?>
+            <div class="product-images-container">
+    <div class="featured-image">
+        <!-- Featured Image -->
+        <?php if ( has_post_thumbnail() ) : ?>
+            <div class="main-image">
+                <?php the_post_thumbnail('full'); // Display the main featured image ?>
+            </div>
+        <?php else: ?>
+            <img src="path-to-default-image.jpg" alt="No gallery image" class="default-featured-image"> <!-- Fallback image if no gallery -->
+        <?php endif; ?>
+        
+        <!-- Arrows around Featured Image -->
+        <div class="featured-image-arrows">
+            <button class="prev-featured-image" aria-label="Previous Featured Image">&#10094;</button>
+            <button class="next-featured-image" aria-label="Next Featured Image">&#10095;</button>
+        </div>
+    </div>
 
-                        <!-- Arrows around Featured Image -->
-                        <div class="featured-image-arrows">
-                            <button class="prev-featured-image" aria-label="Previous Featured Image">&#10094;</button>
-                            <button class="next-featured-image" aria-label="Next Featured Image">&#10095;</button>
-                        </div>
-                    </div>
-
-                    <?php if ($product->get_gallery_image_ids()): ?>
-                        <!-- Product Gallery (Only show if gallery images exist) -->
-                        <div class="product-gallery owl-carousel">
-                            <?php
-                            // Get the product gallery images
-                            $attachment_ids = $product->get_gallery_image_ids();
-                            foreach ($attachment_ids as $attachment_id):
-                                $image_link = wp_get_attachment_url($attachment_id); // Get image URL
-                                ?>
-                                <div class="gallery-item">
-                                    <img src="<?php echo esc_url($image_link); ?>" alt="Product Image">
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
+    <?php if ( $product->get_gallery_image_ids() ) : ?>
+        <!-- Product Gallery (Only show if gallery images exist) -->
+        <div class="product-gallery owl-carousel">
+            <?php
+            // Get the product gallery images
+            $attachment_ids = $product->get_gallery_image_ids();
+            foreach ( $attachment_ids as $attachment_id ) :
+                $image_link = wp_get_attachment_url( $attachment_id ); // Get image URL
+                ?>
+                <div class="gallery-item">
+                    <img src="<?php echo esc_url( $image_link ); ?>" alt="Product Image">
                 </div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+</div>
             </div>
         </div>
     </div>
