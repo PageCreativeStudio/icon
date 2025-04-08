88256b7bbe1999d2c55e377d63a375653ee85a40
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-08 12:50:51 +0100

    Updated -v1.1

--- pagecreative/woocommerce/content-single-product.php
+++ pagecreative/woocommerce/content-single-product.php
@@ -20,11 +20,11 @@ global $product;
             </div>
             <div class="col-12 col-lg pl-lg-5">
                 <div class="d-flex flex-wrap justify-content-between pb-0">
-                    <p class="font-15 mb-0">Continental ICP-01</p>
-                    <p class="font-15 mb-0">SKU25365</p>
+                    <p class="font-16 mb-0">Continental ICP-01</p>
+                    <p class="font-16 mb-0">SKU25365</p>
                 </div>
                 <h1 class="font-30 font-mb-25 my-2"><?php the_title(); ?></h1>
-                <h2 class="font-16">From £4.80/unit</h2>
+                <h2 class="font-18">From £4.80/unit</h2>
             </div>
         </div>
     </div>
