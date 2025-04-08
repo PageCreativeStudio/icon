cf1785f3d580ad53f20158572787fa911182d215
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-08 14:26:14 +0100

    Updated -v1.1

--- pagecreative/woocommerce/content-single-product.php
+++ pagecreative/woocommerce/content-single-product.php
@@ -138,56 +138,57 @@ global $product;
 <!--<div id="product-<?php the_ID(); ?>" <?php wc_product_class('custom-product', $product); ?>>
     <div class="container py-5">
         <div class="row">
-            
+        
             <div class="col-md-6">
                 <div class="product-images">
                     <?php
                     // WooCommerce Product Images (gallery and main image)
-                    do_action('woocommerce_before_single_product_summary');
+//                 do_action('woocommerce_before_single_product_summary');
                     ?>
                 </div>
             </div>
 
+          
             <div class="col-md-6">
                 <div class="product-details">
                     <h1 class="product-title"><?php the_title(); ?></h1>
 
                     <div class="product-price">
                         <?php
-                        do_action('woocommerce_single_product_summary');
+ //                       do_action('woocommerce_single_product_summary');
                         ?>
                     </div>
 
                     <div class="product-description mt-3">
                         <?php
-                        echo wpautop($product->get_short_description());
+//                        echo wpautop($product->get_short_description());
                         ?>
                     </div>
 
                     <div class="product-attributes mt-3">
-                        <?php
-                        $terms = get_the_terms(get_the_ID(), 'pa_colour');
-                        if ($terms && !is_wp_error($terms)) {
-                            echo '<strong>Colour:</strong><br>';
-                            foreach ($terms as $term) {
-                                echo '<span class="badge bg-primary mr-2">' . esc_html($term->name) . '</span>';
-                            }
-                        }
-                        ?>
+ //                       <?php
+ //                       $terms = get_the_terms(get_the_ID(), 'pa_colour');
+ //                       if ($terms && !is_wp_error($terms)) {
+ //                           echo '<strong>Colour:</strong><br>';
+ //                           foreach ($terms as $term) {
+ //                               echo '<span class="badge bg-primary mr-2">' . esc_html($term->name) . '</span>';
+ //                           }
+ //                       }
+ //                       ?>
                     </div>
                 </div>
             </div>
         </div>
 
-    
+       
         <div class="row mt-5">
             <div class="col-12">
                 <?php
                 // WooCommerce Product Tabs (Reviews, Additional Information)
-                do_action('woocommerce_after_single_product_summary');
+//                do_action('woocommerce_after_single_product_summary');
                 ?>
             </div>
         </div>
 
     </div>
-</div>-->
+</div>
