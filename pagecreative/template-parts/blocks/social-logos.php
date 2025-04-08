c78baefdb3668b8b0476ecb142908ee686e5c25a
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-08 14:43:12 +0100

    Updated -v1.1

--- pagecreative/template-parts/blocks/social-logos.php
+++ pagecreative/template-parts/blocks/social-logos.php
@@ -2,7 +2,7 @@
     <div class="socialarea max-width mx-auto text-center pb-3 pb-lg-4">
         <div class="socialarea__container">
             <h2 class="font-28 text-dark font-mb-22 mb-0 pb-5 pb-lg-5 pt-2 px-2">
-                <?php echo get_field('social_proof_title'); ?></h2>
+                <?php echo get_field('social_proof_title', 'options'); ?></h2>
             <div class="d-flex flex-wrap justify-content-center">
                 <?php if (have_rows('logos_repeater')) { ?>
                     <?php while (have_rows('logos_repeater', 'options')) {
