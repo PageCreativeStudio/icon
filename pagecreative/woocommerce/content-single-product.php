531429836902836e56a11b08f32a95761544bf9b
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-08 10:11:15 +0100

    Updated -v1.1

--- pagecreative/woocommerce/content-single-product.php
+++ pagecreative/woocommerce/content-single-product.php
@@ -1,76 +1,85 @@
 <?php
 /**
- * The template for displaying product content in the single-product.php template
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-single-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 3.6.0
+ * Custom Single Product Template
+ * This is a completely custom layout for displaying product details.
  */
 
-defined( 'ABSPATH' ) || exit;
+defined( 'ABSPATH' ) || exit;  // Exit if accessed directly.
 
 global $product;
-
-/**
- * Hook: woocommerce_before_single_product.
- *
- * @hooked woocommerce_output_all_notices - 10
- */
-do_action( 'woocommerce_before_single_product' );
-
-if ( post_password_required() ) {
-	echo get_the_password_form(); // WPCS: XSS ok.
-	return;
-}
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
-	<?php
-	/**
-	 * Hook: woocommerce_before_single_product_summary.
-	 *
-	 * @hooked woocommerce_show_product_sale_flash - 10
-	 * @hooked woocommerce_show_product_images - 20
-	 */
-	do_action( 'woocommerce_before_single_product_summary' );
-	?>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'custom-product', $product ); ?>>
+    <div class="container py-5">
+        <div class="row">
+            <!-- Left Column: Product Images -->
+            <div class="col-md-6">
+                <div class="product-images">
+                    <?php
+                    // WooCommerce Product Images (gallery and main image)
+                    do_action( 'woocommerce_before_single_product_summary' );
+                    ?>
+                </div>
+            </div>
 
-	<div class="summary entry-summary">
-		<?php
-		/**
-		 * Hook: woocommerce_single_product_summary.
-		 *
-		 * @hooked woocommerce_template_single_title - 5
-		 * @hooked woocommerce_template_single_rating - 10
-		 * @hooked woocommerce_template_single_price - 10
-		 * @hooked woocommerce_template_single_excerpt - 20
-		 * @hooked woocommerce_template_single_add_to_cart - 30
-		 * @hooked woocommerce_template_single_meta - 40
-		 * @hooked woocommerce_template_single_sharing - 50
-		 * @hooked WC_Structured_Data::generate_product_data() - 60
-		 */
-		do_action( 'woocommerce_single_product_summary' );
-		?>
-	</div>
+            <!-- Right Column: Product Details -->
+            <div class="col-md-6">
+                <div class="product-details">
+                    <h1 class="product-title"><?php the_title(); ?></h1>
 
-	<?php
-	/**
-	 * Hook: woocommerce_after_single_product_summary.
-	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15
-	 * @hooked woocommerce_output_related_products - 20
-	 */
-	do_action( 'woocommerce_after_single_product_summary' );
-	?>
+                    <!-- Product Price -->
+                    <div class="product-price">
+                        <?php
+                        // Display product price
+                        do_action( 'woocommerce_single_product_summary' );
+                        ?>
+                    </div>
+
+                    <!-- Add to Cart Form -->
+                    <div class="add-to-cart">
+                        <?php
+                        // Add to cart button
+                        do_action( 'woocommerce_single_product_summary' );
+                        ?>
+                    </div>
+
+                    <!-- Product Description -->
+                    <div class="product-description mt-3">
+                        <?php
+                        // Display product excerpt/short description
+                        echo wpautop( $product->get_short_description() );
+                        ?>
+                    </div>
+
+                    <!-- Product Attributes (e.g., Colour) -->
+                    <div class="product-attributes mt-3">
+                        <?php
+                        // Display the colour attribute if it exists
+                        $terms = get_the_terms( get_the_ID(), 'pa_colour' );
+                        if ( $terms && ! is_wp_error( $terms ) ) {
+                            echo '<strong>Colour:</strong><br>';
+                            foreach ( $terms as $term ) {
+                                echo '<span class="badge bg-primary mr-2">' . esc_html( $term->name ) . '</span>';
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Product Tabs (Additional Information, Reviews, etc.) -->
+        <div class="row mt-5">
+            <div class="col-12">
+                <?php
+                // WooCommerce Product Tabs (Reviews, Additional Information)
+                do_action( 'woocommerce_after_single_product_summary' );
+                ?>
+            </div>
+        </div>
+
+    </div>
 </div>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+<?php
+do_action( 'woocommerce_after_single_product' );  // Trigger any actions after the product.
--- pagecreative/woocommerce/single-product.php
+++ pagecreative/woocommerce/single-product.php
@@ -1,38 +1,27 @@
 <?php
 /**
- * The Template for displaying all single products
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see         https://woocommerce.com/document/template-structure/
- * @package     WooCommerce\Templates
- * @version     1.6.4
+ * Custom Single Product Page
+ * This page is responsible for calling the custom template for products.
  */
-
-if (!defined('ABSPATH')) {
+ 
+if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-get_header(); ?>
+get_header( 'shop' ); // Shop header
 
-<h1>New template</h1>
+// Trigger action before product content starts
+do_action( 'woocommerce_before_main_content' );
 
-<?php while (have_posts()): ?>
-    <?php the_post(); ?>
+while ( have_posts() ) : the_post();
 
-    <?php wc_get_template_part('content', 'single-product'); ?>
+    // Custom template part for the product page
+    wc_get_template_part( 'woocommerce/content', 'single-product' );
 
-<?php endwhile; // end of the loop. ?>
+endwhile; // end of the loop
 
+// Trigger action after product content ends
+do_action( 'woocommerce_after_main_content' );
 
-
-<?php
-get_footer();
-
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+get_footer( 'shop' ); // Shop footer
+?>
