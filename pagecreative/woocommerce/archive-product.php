657236c8e0754d6b1830b9e343043d30bc43b9c3
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-15 15:02:04 +0100

    15/04

--- pagecreative/woocommerce/archive-product.php
+++ pagecreative/woocommerce/archive-product.php
@@ -6,57 +6,59 @@
 get_header(); ?>
 
 
-<div class="container-fluid mx-auto px-md-4 pt-3 pb-lg-5 mb-lg-5 bordertop">
-    <div class="category__filter py-3 px-0">
-        <?php
-        $filter_shortcode2 = '[searchandfilter id="1102"]';
-        $filter_html = do_shortcode($filter_shortcode2);
-        echo $filter_html;
-        ?>
-    </div>
-    <div class="row">
-        <div class="col-12 col-lg-3">
-            <div class="megafilter-group">
-                <h4 class="text-black font-18">Filter Products</h4>
-                <?php
-                $filter_shortcode = '[searchandfilter id="1102"]';
-                $filter_html = do_shortcode($filter_shortcode);
-                echo $filter_html;
-                ?>
-            </div>
+<div class="shop__container container-fluid mx-auto px-md-4 pt-3 pb-lg-5 mb-lg-5 ">
+    <div class="bordertop">
+        <div class="category__filter py-3 px-0">
+            <?php
+            $filter_shortcode2 = '[searchandfilter id="1102"]';
+            $filter_html = do_shortcode($filter_shortcode2);
+            echo $filter_html;
+            ?>
         </div>
-        <div class="col-12 col-lg-9 postsrow">
-            <div class="row">
-                <?php
-                if (have_posts()):
-                    while (have_posts()):
-                        the_post();
-                        ?>
-                        <div class="col-md-4 col-sm-6">
-                            <!-- Bootstrap Card for Product -->
-                            <div class="card">
-                                <div class="card-body">
-                                    <?php if (has_post_thumbnail()): ?>
-                                        <div class="product-image">
-                                            <?php the_post_thumbnail('medium'); ?>
-                                        </div>
-                                    <?php endif; ?>
+        <div class="row">
+            <div class="col-12 col-lg-3">
+                <div class="megafilter-group">
+                    <h4 class="text-black font-18">Filter Products</h4>
+                    <?php
+                    $filter_shortcode = '[searchandfilter id="1102"]';
+                    $filter_html = do_shortcode($filter_shortcode);
+                    echo $filter_html;
+                    ?>
+                </div>
+            </div>
+            <div class="col-12 col-lg-9 postsrow">
+                <div class="row">
+                    <?php
+                    if (have_posts()):
+                        while (have_posts()):
+                            the_post();
+                            ?>
+                            <div class="col-md-4 col-sm-6">
+                                <!-- Bootstrap Card for Product -->
+                                <div class="card">
+                                    <div class="card-body">
+                                        <?php if (has_post_thumbnail()): ?>
+                                            <div class="product-image">
+                                                <?php the_post_thumbnail('medium'); ?>
+                                            </div>
+                                        <?php endif; ?>
 
-                                    <h5 class="card-title"><?php the_title(); ?></h5>
-                                    <p class="card-text"><?php echo $product->get_price_html(); ?></p>
+                                        <h5 class="card-title"><?php the_title(); ?></h5>
+                                        <p class="card-text"><?php echo $product->get_price_html(); ?></p>
 
-                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">View Product</a>
+                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">View Product</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <?php
-                    endwhile;
+                            <?php
+                        endwhile;
 
-                    the_posts_navigation();
-                else:
-                    echo 'No Products found.';
-                endif; ?>
+                        the_posts_navigation();
+                    else:
+                        echo 'No Products found.';
+                    endif; ?>
+                </div>
             </div>
         </div>
     </div>
