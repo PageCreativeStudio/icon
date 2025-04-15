587c25d204e00fc5304148335c056b717a7dd02d
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-15 15:03:16 +0100

    15/04

--- pagecreative/woocommerce/archive-product.php
+++ pagecreative/woocommerce/archive-product.php
@@ -16,7 +16,7 @@ get_header(); ?>
             ?>
         </div>
         <div class="row">
-            <div class="col-12 col-lg-3">
+            <div class="col-12 col-lg-3 max-20">
                 <div class="megafilter-group">
                     <h4 class="text-black font-18">Filter Products</h4>
                     <?php
