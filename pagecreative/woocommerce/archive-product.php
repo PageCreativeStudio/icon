95049dfa5fad76a10bf4121661be0ed962e5c777
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-15 11:49:31 +0100

    15/04

--- pagecreative/woocommerce/archive-product.php
+++ pagecreative/woocommerce/archive-product.php
@@ -1,22 +1,32 @@
 <?php
-/**
- * The Template for displaying product archives, including the main shop page which is a post type archive
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 8.6.0
- */
-
 defined( 'ABSPATH' ) || exit;
 
+get_header( 'shop' ); ?>
 
+<main class="container my-5">
+    <h1 class="mb-4"><?php woocommerce_page_title(); ?></h1>
 
-get_footer( 'shop' );
+    <?php do_action( 'woocommerce_before_main_content' ); ?>
+
+    <?php if ( woocommerce_product_loop() ) : ?>
+
+        <?php do_action( 'woocommerce_before_shop_loop' ); ?>
+
+        <div class="row">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <div class="col-md-4 mb-4">
+                    <?php wc_get_template_part( 'content', 'product' ); ?>
+                </div>
+            <?php endwhile; ?>
+        </div>
+
+        <?php do_action( 'woocommerce_after_shop_loop' ); ?>
+
+    <?php else : ?>
+        <?php do_action( 'woocommerce_no_products_found' ); ?>
+    <?php endif; ?>
+
+    <?php do_action( 'woocommerce_after_main_content' ); ?>
+</main>
+
+<?php get_footer( 'shop' ); ?>
