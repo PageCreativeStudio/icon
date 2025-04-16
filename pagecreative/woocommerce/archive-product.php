52299deb39a30a37eedad6f45299edf82bf0042a
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-16 12:53:04 +0100

    15/04

--- pagecreative/woocommerce/archive-product.php
+++ pagecreative/woocommerce/archive-product.php
@@ -142,7 +142,7 @@ $shop_page_id = wc_get_page_id('shop');
             <div class="row acf-collapsibles justify-content-between">
                 <?php if (have_rows('collaspsibles_repeater', $shop_page_id)): ?>
                     <?php while (have_rows('collaspsibles_repeater', $shop_page_id)): the_row(); ?>
-                        <div class="col-lg-5 mb-2">
+                        <div class="col-lg-55 mb-2">
                             <div class="acf-toggle-item text-left">
                                 <h3 class="toggle-header font-16"><?php echo get_sub_field('title'); ?></h3>
                                 <div class="toggle-content">
