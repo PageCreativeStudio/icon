29bdd23c8b021e20c1562e8e6118c71c37c775f3
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-24 12:19:48 +0100

    Updated

--- pagecreative/woocommerce/archive-product.php
+++ pagecreative/woocommerce/archive-product.php
@@ -30,7 +30,7 @@ get_header(); ?>
 <div class="shop__container container-fluid mx-auto px-md-4 pt-3 pb-4 pb-lg-5 mb-lg-5 ">
     <div class="bordertop">
         <div class="d-lg-none d-flex flex-wrap justify-content-between pt-4">
-            <div class="">
+            <div class="align-content-center">
                 <p class="text-black font-15 mb-0">Product Categories</p>
             </div>
             <div class="">
