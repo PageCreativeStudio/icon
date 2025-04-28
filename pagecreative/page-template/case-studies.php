ce02b24fca9a0c865ae83b0443d49b61134f56ee
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-28 13:49:51 +0100

    Updated wp-admin

--- pagecreative/page-template/case-studies.php
+++ pagecreative/page-template/case-studies.php
@@ -27,7 +27,7 @@ get_header(); ?>
                             <img class="w-100" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>"
                                 alt="<?php the_title(); ?>">
                         </div>
-                        <h2 class="text-dark font-25 font-mb-22 pt-3 pb-2">
+                        <h2 class="text-dark font-25 font-mb-22 pt-3 pb-0 mb-2">
                             <?php the_title(); ?>
                         </h2>
                         <p class="font-15">
