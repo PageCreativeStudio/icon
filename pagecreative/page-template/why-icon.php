9e8c45e1a4965fc0770f9ce0b68d2f0e69e2460e
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 16:11:26 +0100

    why GB blocks

--- pagecreative/page-template/why-icon.php
+++ pagecreative/page-template/why-icon.php
@@ -6,7 +6,7 @@
 get_header(); ?>
 
 
-<div class="container-fluid mx-auto px-md-4 mx-auto text-left pb-5 pt-lg-4">
+<div class="container-fluid mx-auto px-md-4 mx-auto text-left pb-5 pt-3 pt-lg-4">
     <h2 class="bordertop borderbottom font-18 font-mb-16 mb-lg-3 py-3">We just care a lot more</h2>
 </div>
 
