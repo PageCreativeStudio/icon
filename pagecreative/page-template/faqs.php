80d0ef0ac6538e99416fa46009e5937db6308d80
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 13:07:56 +0100

    Updated wp-admin

--- pagecreative/page-template/faqs.php
+++ pagecreative/page-template/faqs.php
@@ -7,7 +7,7 @@ get_header(); ?>
 
 
 <div class="faqs container-fluid mx-auto px-md-4 mx-auto text-left">
-    <h2 class="bordertop borderbottom font-22 font-mb-16 mb-lg-3 py-3 mt-lg-3">Frequently Asked Questions</h2>
+    <h2 class="bordertop borderbottom font-20 font-mb-16 mb-lg-3 py-3 mt-lg-3">Frequently Asked Questions</h2>
 
 </div>
 
