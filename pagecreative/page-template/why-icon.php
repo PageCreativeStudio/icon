286c71beb9c36e98858b3c33074e03464e8bd6c3
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 14:54:36 +0100

    Updated faqs

--- pagecreative/page-template/why-icon.php
+++ pagecreative/page-template/why-icon.php
@@ -7,35 +7,8 @@ get_header(); ?>
 
 
 <div class="faqs container-fluid mx-auto px-md-4 mx-auto text-left pb-5">
-    <h2 class="bordertop borderbottom font-20 font-mb-16 mb-lg-3 py-3 mt-lg-3">Frequently Asked Questions</h2>
-    <div class="row justify-content-between pb-lg-4 pt-3 pt-lg-4">
-        <div class="col-12 col-lg-4 order-lg-1 order-2 h-100">
-            <img class="w-100 h-100 mb-lg-4 mt-5 mt-lg-0" src="<?php echo get_field('featured_image'); ?>"
-                alt="Frequently Asked Questions">
-            <div class="font-15 singlecontent bordertop pt-4 mt-5">
-                <?php echo get_field('content_editor'); ?>
-            </div>
-        </div>
-        <div class="col-12 col-lg-8 order-lg-2 order-1 pl-lg-4">
-            <div class="stickytop">
-                <?php if (have_rows('faqs_repeater')) { ?>
-                    <?php while (have_rows('faqs_repeater')) {
-                        the_row(); ?>
-                        <div class="faq-item">
-                            <h3 class="borderbottom py-3 mb-0 font-18 font-mb-16 mb-lg-3 pr-4 pr-lg-3">
-                                <?php echo get_sub_field('question'); ?>
-                            </h3>
-                            <div class="togglearea">
-                                <div class="py-2 px-1">
-                                    <?php echo get_sub_field('answer'); ?>
-                                </div>
-                            </div>
-                        </div>
-                    <?php } ?>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
+    <h2 class="bordertop borderbottom font-20 font-mb-16 mb-lg-3 py-3 mt-lg-3">We just care a lot more</h2>
+
 </div>
 
 <?php get_template_part('template-parts/blocks/social-logos'); ?>
@@ -44,12 +17,15 @@ get_header(); ?>
     <?php get_template_part('template-parts/blocks/testimonials'); ?>
 </div>
 
-<div class="font-15 singlecontent bordertop pt-4 mt-5 pb-5">
-    <p class="font-15 text-gray max-20 pt-2">
-        <span class="text-black">Not finding what you're looking for?</span><br><br>
-        Can't find what you're looking for or need some assistance? Call us on 0207 183 8431 or send us an email.
-    </p>
+<div class="container-fluid mx-auto px-md-4">
+    <div class="font-15 singlecontent bordertop pt-4 mt-5 pb-5">
+        <p class="font-15 text-gray max-25 pt-2">
+            <span class="text-black">Not finding what you're looking for?</span><br><br>
+            Can't find what you're looking for or need some assistance? Call us on 0207 183 8431 or send us an email.
+        </p>
+    </div>
 </div>
+
 
 
 <?php get_footer(); ?>
