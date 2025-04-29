496584182b3b384d24369d6fe940deb43a7cba93
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 16:09:55 +0100

    why GB blocks

--- pagecreative/template-parts/blocks/vertical-hero.php
+++ pagecreative/template-parts/blocks/vertical-hero.php
@@ -7,7 +7,7 @@
                     <div class="verticalcol col-12 col-lg-6 pb-4 pb-lg-4">
                         <div class="row">
                             <div class="col-12">
-                                <div class="postarea__image-container h-100">
+                                <div class="postarea__image-container h-100 pt-3 pt-lg-0">
                                     <img class="w-100" src="<?php echo get_sub_field('two_col_image'); ?>"
                                         alt="<?php echo get_sub_field('two_col_title'); ?>">
                                     <?php if (get_sub_field("two_col_image_caption")): ?>
