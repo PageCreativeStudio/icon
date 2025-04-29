9e0e16a1825a3500d0276284f86a0a6c49d3cd63
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 15:27:17 +0100

    why-uGB blocks

--- pagecreative/template-parts/blocks/hero-header.php
+++ pagecreative/template-parts/blocks/hero-header.php
@@ -2,22 +2,26 @@
     <div class="hero-header container-fluid mx-auto px-md-4 mx-auto pb-3 pb-lg-4">
         <div class="stickyrow row">
             <div class="col-12 col-lg-4 px-3 order-2 order-lg-1">
-                <a class="align-self-center" href="<?php the_permalink(); ?>">
-                    <div class="pt-3 pt-lg-3 mt-lg-2">
-                        <span class="font-14 font-mb-12 uppercase text-gray mb-0">
-                            <span><?php echo esc_html(implode(', ', $cat_names)); ?></span>
-                        </span>
-                        <h2 class="text-black font-30 font-mb-20 mt-2 mt-lg-2 pb-0 mb-1 max-30">
-                            <?php the_title(); ?>
-                        </h2>
-                    </div>
-                    <span class="text-sec font-13 underline align-self-center">Read more</span>
-                </a>
+                <div class="pt-3 pt-lg-3 mt-lg-2">
+                    <span class="font-14 font-mb-12 uppercase text-gray my-0 pb-3">
+                        <?php echo get_field('mini_title'); ?>
+                    </span>
+                    <h2 class="text-black font-30 font-mb-20 my-0 pb-3 max-30">
+                        <?php echo get_field('two_col_title'); ?>
+                    </h2>
+                    <h3 class="font-20 font-mb-18 text-black my-0 pb-3"><?php echo get_field('two_col_sub_title'); ?>
+                    </h3>
+                    <p class="font-15 text-black my-0 pb-3"><?php echo get_field('two_col_description'); ?></p>
+                    <a href="<?php echo get_field('two_col_btn_link'); ?>"
+                        class="text-sec font-13 underline align-self-center my-0 pt-3"><?php echo get_field('two_col_btn_title'); ?></a>
+                </div>
+
             </div>
             <div class="col-12 col-lg-8 order-1 order-lg-2">
                 <div class="postarea__image-container h-100">
-                    <img class="w-100" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>"
-                        alt="<?php the_title(); ?>">
+                    <img class="w-100 mb-2" src="<?php echo get_field('two_col_image'); ?>"
+                        alt="<?php echo get_field('two_col_title'); ?>">
+                    <p class="font-13 text-gray"><?php echo get_field('two_col_image_caption'); ?></p>
                 </div>
             </div>
         </div>
