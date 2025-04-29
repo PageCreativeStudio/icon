48b5aa33fb473a399a2b719fd1ce43d5c97cced9
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 16:43:03 +0100

    why GB blocks

--- pagecreative/page-template/contact.php
+++ pagecreative/page-template/contact.php
@@ -18,13 +18,13 @@ get_header(); ?>
                     src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="Contact icon printing">
             </div>
         </div>
-        <div class="col-12 col-lg-6 order-1 order-lg-2">
+        <div class="col-12 col-lg-6 order-1 order-lg-2 d-grid">
             <h2 class="font-30 text-black font-mb-25 mb-0 pb-4">We're here to help</h2>
-            <p class="font-16 text-black pb-3 mb-0">You can find answers to the most comon enquiries on our <a
+            <p class="font-18 text-black pb-3 mb-0 max-30">You can find answers to the most comon enquiries on our <a
                     class="underline" href="<?php echo home_url(); ?>/faqs/">FAQ</a>.
                 If you still can't find what you're after, get in touch.</p>
 
-            <span class="w-100 bordertop pt-4">General enquiries</span>
+            <span class="w-100 text-black bordertop pt-4">General enquiries</span>
             <p class="font-15 text-black pb-3 mb-0 pb-4">We're open Monday to Friday — 9am to 6pm</p>
 
             <a class="underline font-18 text-black" href="mailto:sales@iconprinting.com">sales@iconprinting.com</a>
@@ -38,10 +38,10 @@ get_header(); ?>
             <a class="underline font-18 text-black arrowup" href="https://x.com/iconprinting/"
                 target="_blank">Instagram</a>
 
-            <span class="w-100 bordertop pt-4">Address</span>
+            <span class="w-100 bordertop text-black pt-4">Address</span>
             <p class="font-18 text-black mb-0">Unit 1, Ground Floor<br>
                 55 Dalston Lane</p>
-            <span class="w-100 bordertop pt-4">London E8 2NGMeetings are strictly by appointment only</span>
+            <span class="w-100 bordertop text-black pt-4">London E8 2NG Meetings are strictly by appointment only</span>
 
         </div>
     </div>
