d68fbaf9567ba64592db49805909d3804fad0aec
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-29 13:28:07 +0100

    Updated wp-admin

--- pagecreative/page-template/faqs.php
+++ pagecreative/page-template/faqs.php
@@ -20,7 +20,7 @@ get_header(); ?>
                 <?php while (have_rows('faqs_repeater')) {
                     the_row(); ?>
                     <div class="faq-item">
-                        <h3 class="borderbottom py-3 mb-0 font-18 font-mb-16 mt-lg-2"><?php echo get_sub_field('question'); ?></h3>
+                        <h3 class="borderbottom py-3 mb-0 font-18 font-mb-16 mt-lg-3"><?php echo get_sub_field('question'); ?></h3>
                         <div class="togglearea">
                             <div class="py-2 px-2">
                             <?php echo get_sub_field('answer'); ?>
