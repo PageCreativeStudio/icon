776c6719210ebe3e9a6d398866ea27d403985911
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 13:20:49 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/images-with-caption.php
+++ pagecreative/template-parts/blocks/images-with-caption.php
@@ -3,19 +3,19 @@
         <div class="verticalcol row">
             <div class="col-12">
                 <div class="postarea__image-container h-100">
-                    <img class="w-100" src="<?php echo get_sub_field('two_col_image_1'); ?>"
-                        alt="<?php echo get_sub_field('two_col_image_1_caption'); ?>">
-                    <?php if (get_sub_field("two_col_image_1_caption")): ?>
-                        <p class="font-13 text-gray pt-3"><?php echo get_sub_field('two_col_image_1_caption'); ?></p>
+                    <img class="w-100" src="<?php echo get_field('two_col_image_1'); ?>"
+                        alt="<?php echo get_field('two_col_image_1_caption'); ?>">
+                    <?php if (get_field("two_col_image_1_caption")): ?>
+                        <p class="font-13 text-gray pt-3"><?php echo get_field('two_col_image_1_caption'); ?></p>
                     <?php endif; ?>
                 </div>
             </div>
             <div class="col-12">
                 <div class="postarea__image-container h-100">
-                    <img class="w-100" src="<?php echo get_sub_field('two_col_image_2'); ?>"
-                        alt="<?php echo get_sub_field('two_col_image_2_caption'); ?>">
-                    <?php if (get_sub_field("two_col_image_2_caption")): ?>
-                        <p class="font-13 text-gray pt-3"><?php echo get_sub_field('two_col_image_2_caption'); ?></p>
+                    <img class="w-100" src="<?php echo get_field('two_col_image_2'); ?>"
+                        alt="<?php echo get_field('two_col_image_2_caption'); ?>">
+                    <?php if (get_field("two_col_image_2_caption")): ?>
+                        <p class="font-13 text-gray pt-3"><?php echo get_field('two_col_image_2_caption'); ?></p>
                     <?php endif; ?>
                 </div>
             </div>
