8a77b0812bd2bc1a630cf11ca6d4300bb84f0a3f
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 12:20:45 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/text-editor.php
+++ pagecreative/template-parts/blocks/text-editor.php
@@ -1,9 +1,9 @@
 <section class="pb-5 pt-2">
-    <div class="faqs container-fluid max-50 mx-auto px-md-4 mx-auto text-left pb-4 pb-lg-4">
+    <div class="faqs container-fluid max-50 mx-auto px-md-4 mx-auto text-left pb-4 pb-lg-5">
         <div class="row">
             <div class="col-12">
                 <?php if (get_field("text_editor_title")): ?>
-                    <h3 class="font-20 font-mb-17 text-black my-0 pb-3">
+                    <h3 class="font-25 font-mb-18 text-black my-0 pb-3">
                         <?php echo get_field('text_editor_title'); ?>
                     </h3>
                 <?php endif; ?>
