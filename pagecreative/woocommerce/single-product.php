7dde113b8f2e5e23df9c214069fe2b2499adc547
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 10:19:18 +0100

    Services cpt

--- pagecreative/woocommerce/single-product.php
+++ pagecreative/woocommerce/single-product.php
@@ -8,14 +8,44 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-get_header();
+get_header(); ?>
 
-// Trigger action before product content starts
-//do_action( 'woocommerce_before_main_content' );
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
 
-while (have_posts()):
-    the_post();
-    wc_get_template_part('woocommerce/content', 'single-product');
-endwhile; 
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				pagecreative_posted_on();
+				pagecreative_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
 
-get_footer(); ?>
+	<?php pagecreative_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+		while (have_posts()):
+            the_post();
+            wc_get_template_part('woocommerce/content', 'single-product');
+        endwhile; 
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php pagecreative_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
+
+
+<?php get_footer(); ?>
+
