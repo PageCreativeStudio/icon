edcfbcb8ab943debec6d34306d57e5b81f07a70d
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 10:22:40 +0100

    Services cpt

--- pagecreative/woocommerce/single-product.php
+++ pagecreative/woocommerce/single-product.php
@@ -11,7 +11,27 @@ if (!defined('ABSPATH')) {
 get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-   
+    <header class="entry-header">
+        <?php
+        if (is_singular()):
+            the_title('<h1 class="entry-title">', '</h1>');
+        else:
+            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+        endif;
+
+        if ('post' === get_post_type()):
+            ?>
+            <div class="entry-meta">
+                <?php
+                pagecreative_posted_on();
+                pagecreative_posted_by();
+                ?>
+            </div>
+        <?php endif; ?>
+    </header>
+
+    <?php pagecreative_post_thumbnail(); ?>
+
     <div class="entry-content">
         <?php
         while (have_posts()):
