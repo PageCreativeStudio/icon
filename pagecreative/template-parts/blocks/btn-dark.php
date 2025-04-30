d491256930f299b01315ce8b561c0aa1f2cbe69b
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 11:32:14 +0100

    Gutenberg blocks

---
+++ pagecreative/template-parts/blocks/btn-dark.php
@@ -0,0 +1,4 @@
+<div class="container-fluid mx-auto text-center pb-5">
+    <a class="btnc bg-dark font-15 text-white px-4 px-lg-5" href="<?php echo get_field('btn_dark_link'); ?>">
+        <?php echo get_field('btn_dark_label'); ?> </a>
+</div>
