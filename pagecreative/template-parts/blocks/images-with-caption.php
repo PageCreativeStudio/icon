368ed3a637a88a4cb94be88a85f9f1414f7fc96b
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 13:24:05 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/images-with-caption.php
+++ pagecreative/template-parts/blocks/images-with-caption.php
@@ -1,8 +1,8 @@
 <section class="pb-5">
     <div class="container-fluid mx-auto px-md-4 mx-auto pb-2 pb-lg-4">
         <div class="verticalcol row">
-            <div class="col-12">
-                <div class="postarea__image-container h-100">
+            <div class="col-12 col-lg-6">
+                <div class="postarea__image-container h-100 text-left">
                     <img class="w-100" src="<?php echo get_field('two_col_image_1'); ?>"
                         alt="<?php echo get_field('two_col_image_1_caption'); ?>">
                     <?php if (get_field("two_col_image_1_caption")): ?>
@@ -10,8 +10,8 @@
                     <?php endif; ?>
                 </div>
             </div>
-            <div class="col-12">
-                <div class="postarea__image-container h-100">
+            <div class="col-12 col-lg-6">
+                <div class="postarea__image-container h-100 text-left">
                     <img class="w-100" src="<?php echo get_field('two_col_image_2'); ?>"
                         alt="<?php echo get_field('two_col_image_2_caption'); ?>">
                     <?php if (get_field("two_col_image_2_caption")): ?>
