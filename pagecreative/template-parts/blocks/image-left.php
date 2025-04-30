88eb839cebb07faa916c105e86182dbc20703b32
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 13:11:25 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/image-left.php
+++ pagecreative/template-parts/blocks/image-left.php
@@ -2,7 +2,7 @@
     <div class="container-fluid w-100 mx-auto px-lg-4 mx-auto text-left pb-4 pb-lg-5">
         <div class="row">
             <div class="col-12 col-lg-6 pr-lg-4">
-                <img class="w-100" src="<?php echo get_field('image_left'); ?>" alt="<?php the_title(); ?>">
+                <img class="w-100" src="<?php echo get_field('image_left'); ?>" alt="<?php echo get_field('image_left_title'); ?>">
                 <?php if (get_field("image_left_caption")): ?>
                     <p class="font-13 text-gray pt-3 mb-1 mb-lg-3"><?php echo get_field('image_left_caption'); ?></p>
                 <?php endif; ?>
