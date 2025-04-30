7d2a22f7e34991a6964fda17041c95741cfeb81e
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 12:57:50 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/image-left.php
+++ pagecreative/template-parts/blocks/image-left.php
@@ -8,15 +8,17 @@
                 <?php endif; ?>
             </div>
             <div class="col-12 col-lg-6">
-                <?php if (get_field("image_left_title")): ?>
-                    <h2 class="font-25 font-mb-18 text-black my-0 pb-3">
-                        <?php echo get_field('image_left_title'); ?>
-                    </h2>
-                <?php endif; ?>
-                <?php if (get_field("image_left_textarea")): ?>
-                    <div class="font-15 font-mb-14 text-black my-0 pb-2"><?php echo get_field('image_left_textarea'); ?>
-                    </div>
-                <?php endif; ?>
+                <div class="max-45">
+                    <?php if (get_field("image_left_title")): ?>
+                        <h2 class="font-25 font-mb-18 text-black my-0 pb-3">
+                            <?php echo get_field('image_left_title'); ?>
+                        </h2>
+                    <?php endif; ?>
+                    <?php if (get_field("image_left_textarea")): ?>
+                        <div class="font-15 font-mb-14 text-black my-0 pb-2"><?php echo get_field('image_left_textarea'); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
