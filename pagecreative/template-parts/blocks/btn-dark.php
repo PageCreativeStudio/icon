59675a3680cf4ca69605f039f988dedb66cff813
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 11:42:02 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/btn-dark.php
+++ pagecreative/template-parts/blocks/btn-dark.php
@@ -1,4 +1,4 @@
-<div class="container-fluid mx-auto text-center pb-5">
+<div class="container-fluid mx-auto text-center pb-5 mb-4 mb-lg-5">
     <a class="btnc bg-dark font-15 text-white px-4 px-lg-5" href="<?php echo get_field('btn_dark_link'); ?>">
         <?php echo get_field('btn_dark_label'); ?> </a>
 </div>
--- pagecreative/template-parts/blocks/btn-light.php
+++ pagecreative/template-parts/blocks/btn-light.php
@@ -1,4 +1,4 @@
-<div class="container-fluid mx-auto text-center pb-5">
+<div class="container-fluid mx-auto text-center pb-5 mb-4 mb-lg-5">
     <a class="btna bg-white font-15 text-center text-black px-4 px-lg-5" href="<?php echo get_field('btn_light_link'); ?>">
         <?php echo get_field('btn_light_label'); ?>
     </a>
