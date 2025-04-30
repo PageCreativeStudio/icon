59edb0db5d2847a8fba10261d6a2b3bf60dc79d0
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 12:56:45 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/image-left.php
+++ pagecreative/template-parts/blocks/image-left.php
@@ -1,6 +1,6 @@
 <section class="pb-5 pt-2 imagewithtext">
     <div class="container-fluid mx-auto px-lg-4 mx-auto text-left pb-4 pb-lg-5">
-        <div>
+        <div class="row">
             <div class="col-12 col-lg-6 pl-lg-5">
                 <img class="w-100" src="<?php echo get_field('image_left'); ?>" alt="<?php the_title(); ?>">
                 <?php if (get_field("image_left_caption")): ?>
