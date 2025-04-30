84fcd35095f099d1430958d9a98cc532edd8668f
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 11:40:20 +0100

    Gutenberg blocks

--- pagecreative/template-parts/blocks/btn-light.php
+++ pagecreative/template-parts/blocks/btn-light.php
@@ -1,5 +1,5 @@
 <div class="container-fluid mx-auto text-center pb-5">
-    <a class="btnb bg-white font-15 text-center text-black px-5" href="<?php echo get_field('btn_light_link'); ?>">
+    <a class="btna bg-white font-15 text-center text-black px-5" href="<?php echo get_field('btn_light_link'); ?>">
         <?php echo get_field('btn_light_label'); ?>
     </a>
 </div>
