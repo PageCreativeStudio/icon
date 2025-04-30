44eacc6e43dc2bc1553fdaaddb14f0db19ac5d3c
PageCreativeStudio <web@pagecreative.co.uk>
2025-04-30 10:23:19 +0100

    Services cpt

--- pagecreative/woocommerce/single-product.php
+++ pagecreative/woocommerce/single-product.php
@@ -10,40 +10,43 @@ if (!defined('ABSPATH')) {
 
 get_header(); ?>
 
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <?php
-        if (is_singular()):
-            the_title('<h1 class="entry-title">', '</h1>');
-        else:
-            the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-        endif;
+	<header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		endif;
 
-        if ('post' === get_post_type()):
-            ?>
-            <div class="entry-meta">
-                <?php
-                pagecreative_posted_on();
-                pagecreative_posted_by();
-                ?>
-            </div>
-        <?php endif; ?>
-    </header>
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+				<?php
+				pagecreative_posted_on();
+				pagecreative_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
 
-    <?php pagecreative_post_thumbnail(); ?>
+	<?php pagecreative_post_thumbnail(); ?>
 
-    <div class="entry-content">
-        <?php
-        while (have_posts()):
+	<div class="entry-content">
+		<?php
+		while (have_posts()):
             the_post();
             wc_get_template_part('woocommerce/content', 'single-product');
-        endwhile;
-        ?>
-    </div>
+        endwhile; 
+		?>
+	</div><!-- .entry-content -->
 
-    <footer class="entry-footer">
-        <?php pagecreative_entry_footer(); ?>
-    </footer>
-</article><?php the_ID(); ?>
+	<footer class="entry-footer">
+		<?php pagecreative_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
+
 
 <?php get_footer(); ?>
+
