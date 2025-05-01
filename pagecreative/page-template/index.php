be4176ec7d76c7694f5cd20e674384db2a2c31f0
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-01 11:42:33 +0100

    home

--- pagecreative/page-template/index.php
+++ pagecreative/page-template/index.php
@@ -88,7 +88,7 @@ get_header(); ?>
                             <?php } ?>
                         <?php } ?>
                     </ul>
-                    <div class="d-flex flex-wrap align-content-center mt-3">
+                    <div class="d-flex flex-wrap align-content-center mt-2">
                         <a href="<?php echo get_field('button_dark_link_1'); ?>"
                             class="btnc bg-dark font-15 text-white mr-3 mb-0"><?php echo get_field('button_dark_label_1'); ?></a>
                         <a href="<?php echo get_field('button_light_link_1'); ?>"
