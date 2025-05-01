8bbb87c4120a5ff964cd959f559ed7641526627d
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-01 12:35:51 +0100

    home

--- pagecreative/page-template/index.php
+++ pagecreative/page-template/index.php
@@ -183,7 +183,7 @@ get_header(); ?>
 <section class="pb-4 pb-lg-5 pt-0 imageswith__text">
     <div class="container-fluid w-100 mx-auto px-lg-4 mx-auto text-left pb-4 pb-lg-5">
         <div class="row">
-            <div class="col-12 col-lg-6 pr-lg-4 twoimages">
+            <div class="col-12 col-lg-6 pr-lg-5 twoimages">
                 <div class="d-flex h-100">
                     <img class="twoimages__2" src="<?php echo get_field('image_left_1'); ?>"
                         alt="<?php echo get_field('image_left_title'); ?>">
