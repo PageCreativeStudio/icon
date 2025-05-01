3af02c0c11a7f1ea1e2a1e380950f7303798b4ae
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-01 12:33:47 +0100

    home

--- pagecreative/page-template/index.php
+++ pagecreative/page-template/index.php
@@ -62,9 +62,9 @@ get_header(); ?>
         <div class="row">
             <div class="col-12 col-lg-6 pl-lg-4 twoimages order-1 order-lg-2 ">
                 <div class="d-flex h-100">
-                    <img class="" src="<?php echo get_field('image_right_1'); ?>"
+                    <img class="twoimages__1" src="<?php echo get_field('image_right_1'); ?>"
                         alt="<?php echo get_field('image_right_title'); ?>">
-                    <img class="d-none d-lg-flex" src="<?php echo get_field('image_right_2'); ?>"
+                    <img class="d-none d-lg-flex twoimages__2" src="<?php echo get_field('image_right_2'); ?>"
                         alt="<?php echo get_field('image_right_title'); ?>">
                 </div>
             </div>
@@ -183,11 +183,11 @@ get_header(); ?>
 <section class="pb-4 pb-lg-5 pt-0 imageswith__text">
     <div class="container-fluid w-100 mx-auto px-lg-4 mx-auto text-left pb-4 pb-lg-5">
         <div class="row">
-            <div class="col-12 col-lg-6 pl-lg-4 twoimages">
+            <div class="col-12 col-lg-6 pr-lg-4 twoimages">
                 <div class="d-flex h-100">
-                    <img class="" src="<?php echo get_field('image_left_1'); ?>"
+                    <img class="twoimages__2" src="<?php echo get_field('image_left_1'); ?>"
                         alt="<?php echo get_field('image_left_title'); ?>">
-                    <img class="d-none d-lg-flex" src="<?php echo get_field('image_left_2'); ?>"
+                    <img class="d-none d-lg-flex twoimages__1" src="<?php echo get_field('image_left_2'); ?>"
                         alt="<?php echo get_field('image_left_title'); ?>">
                 </div>
             </div>
