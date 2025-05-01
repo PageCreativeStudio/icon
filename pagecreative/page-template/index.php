362f1aae239548fcaed069fb30dfa11ce392ab3a
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-01 11:31:32 +0100

    Gutenberg blocks

--- pagecreative/page-template/index.php
+++ pagecreative/page-template/index.php
@@ -63,31 +63,34 @@ get_header(); ?>
         <div class="row">
             <div class="col-12 col-lg-6 pl-lg-4 twoimages order-1 order-lg-2 ">
                 <div class="d-flex">
-                    <img class="w-100"
-                        src="https://pagedev.co.uk/clients/icon/wp-content/uploads/2025/05/05308efe-fd0a-4ff8-a65f-16446aacd241_Look1_6928129.jpg.png"
+                    <img class="w-100" src="<?php echo get_field('image_right_1'); ?>"
                         alt="<?php echo get_field('image_right_title'); ?>">
-                    <img class="w-100 d-none d-lg-flex"
-                        src="https://pagedev.co.uk/clients/icon/wp-content/uploads/2025/05/05308efe-fd0a-4ff8-a65f-16446aacd241_Look1_6928129.jpg.png"
+                    <img class="w-100 d-none d-lg-flex" src="<?php echo get_field('image_right_2'); ?>"
                         alt="<?php echo get_field('image_right_title'); ?>">
                 </div>
             </div>
             <div class="col-12 col-lg-6 order-2 order-lg-1 pt-3 pt-lg-0 align-content-center">
                 <div class="max-45">
                     <h2 class="font-30 font-mb-22 text-black my-0 pb-3">
-                        Level up your merch with ICON
+                        <?php echo get_field('image_right_title'); ?>
                     </h2>
-                    <p class="font-16 font-mb-15 text-gray my-0 pb-2">Custom merch gets a bad rap. For good reason: it's
-                        often cheaply made, badly designed,
-                        and quickly headed for the bin. We're here to change all that. ICON Printing is based in
-                        London, and uses the highest quality garments available for our printing, so you can be
-                        sure of merch to take pride in.
+                    <p class="font-16 font-mb-15 text-gray my-0 pb-2">
+                        <?php echo get_field('image_right_description'); ?>
                     </p>
-                    <ul class="m-0 p-0">
-                        <li class="d-flex"><img src="" alt="">Multiple printing methods</li>
-                    </ul>
+                    <?php if (have_rows('list_repeater_right')) { ?>
+                        <?php while (have_rows('list_repeater_right')) {
+                            the_row(); ?>
+                            <ul class="m-0 p-0 listicons">
+                                <li class="d-flex"><img src="<?php echo get_sub_field('icon'); ?>"
+                                        alt="<?php echo get_sub_field('text'); ?>"><?php echo get_sub_field('text'); ?></li>
+                            </ul>
+                        <?php } ?>
+                    <?php } ?>
                     <div class="d-flex flex-wrap align-content-center mt-3">
-                        <a href="#" class="btnc bg-dark font-15 text-white mr-3 mb-0">Our Services</a>
-                        <a href="" class="btna mb-0">Get a quote</a>
+                        <a href="<?php echo get_field('button_dark_link_1'); ?>"
+                            class="btnc bg-dark font-15 text-white mr-3 mb-0"><?php echo get_field('button_dark_label_1'); ?></a>
+                        <a href="<?php echo get_field('button_light_link_1'); ?>"
+                            class="btna mb-0"><?php echo get_field('button_light_label_1'); ?></a>
                     </div>
                 </div>
             </div>
