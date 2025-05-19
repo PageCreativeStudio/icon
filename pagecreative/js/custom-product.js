5f5682cbb75107ae7e7f96eb0630bc6cac1309be
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-19 12:30:27 +0100

    Updated woocommerce

--- pagecreative/js/custom-product.js
+++ pagecreative/js/custom-product.js
@@ -1,23 +1,18 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     // Show/hide technique sections based on print area checkboxes
-    $('.print-area-checkbox').on('change', function() {
-        var areaKey = $(this).data('area');
-        var techniqueSection = $('#technique_' + areaKey);
+    $('.print-area-checkbox').on('change', function () {
+        var parentPrintArea = $(this).closest('.print-area');
+
         if ($(this).is(':checked')) {
-            techniqueSection.show();
+            $('.print-area').removeClass('active'); // Remove from all
+            parentPrintArea.addClass('active');     // Add to clicked one
         } else {
-            techniqueSection.hide();
-              // Clear selected print technique
-            $('input[name="print_techniques[' + areaKey + ']"]', techniqueSection).prop('checked', false);
-            // Clear selected technique options for all techniques in this section
-            $('.technique-options input[type="radio"]', techniqueSection).prop('checked', false);
-            // Hide all technique options subsections
-            $('.technique-options', techniqueSection).hide();
+            parentPrintArea.removeClass('active');  // Remove if unchecked
         }
     });
 
     // Show/hide technique options based on selected technique
-    $('.print-technique').on('change', function() {
+    $('.print-technique').on('change', function () {
         var areaKey = $(this).closest('.print-technique-section').attr('id').replace('technique_', '');
         var techniqueKey = $(this).val().toLowerCase().replace(/\s+/g, '');
         $('.technique-options', '#technique_' + areaKey).hide();
@@ -25,11 +20,11 @@ jQuery(document).ready(function($) {
     });
 
     // Trigger change events on page load to ensure correct visibility
-  //  $('.print-area-checkbox').trigger('change');
-  //  $('.print-technique').trigger('change');
+    //  $('.print-area-checkbox').trigger('change');
+    //  $('.print-technique').trigger('change');
 
     // Show upload button when a print area is selected
-    $('.print-area-checkbox').on('change', function() {
+    $('.print-area-checkbox').on('change', function () {
         if ($('.print-area-checkbox:checked').length > 0) {
             $('.upload-button').show();
         } else {
@@ -39,7 +34,7 @@ jQuery(document).ready(function($) {
     });
 
     // Trigger file input click when upload button is clicked
-    $('.upload-button').on('click', function() {
+    $('.upload-button').on('click', function () {
         $('#logo-upload').click();
     });
 
