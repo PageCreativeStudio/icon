98f2f4735311b6ae3859c56e3a14fa5368c74849
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-19 16:51:50 +0100

    Updated woocommerce

--- pagecreative/woocommerce/myaccount/form-edit-account.php
+++ pagecreative/woocommerce/myaccount/form-edit-account.php
@@ -91,7 +91,7 @@ do_action('woocommerce_before_edit_account_form');
 			?>
 
 			<fieldset">
-				<legend class="text-black font-18 pt-5">Password:</legend>
+				<legend class="text-black font-18 pt-5 mt-lg-4">Password:</legend>
 
 				<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 					<!--<label
