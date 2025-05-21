96a070f8b7a999df862e51cde7bdbaa69f0a316f
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-21 09:26:33 +0100

    Updated woocommerce

--- pagecreative/woocommerce/myaccount/navigation.php
+++ pagecreative/woocommerce/myaccount/navigation.php
@@ -15,25 +15,40 @@
  * @version 9.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-do_action( 'woocommerce_before_account_navigation' );
+do_action('woocommerce_before_account_navigation');
 ?>
 
-<div class="container-fluid mx-auto max-100 px-2 px-md-4 mx-auto text-left pb-5 pt-3 pt-lg-4" aria-label="<?php esc_html_e( 'Account pages', 'woocommerce' ); ?>">
-    <div class="bordertop borderbottom font-18 font-mb-16 mb-lg-3 py-3">
-        <ul class="accountnavigation d-grid d-md-flex flex-wrap">
-    		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-    			<li class="<?php echo wc_get_account_menu_item_classes( $endpoint ); ?> pl-2 pr-2">
-    				<a class="pr-2" href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>" <?php echo wc_is_current_account_menu_item( $endpoint ) ? 'aria-current="page"' : ''; ?>>
-    					<?php echo esc_html( $label ); ?>
-    				</a> |
-    			</li>
-    		<?php endforeach; ?>
-    	</ul>
-    </div>
+<div class="container-fluid mx-auto max-100 px-2 px-md-4 mx-auto text-left pb-5 pt-3 pt-lg-4"
+	aria-label="<?php esc_html_e('Account pages', 'woocommerce'); ?>">
+	<div class="bordertop borderbottom font-18 font-mb-16 mb-lg-3 py-3">
+		<ul class="accountnavigation d-grid d-md-flex flex-wrap">
+			<?php foreach (wc_get_account_menu_items() as $endpoint => $label): ?>
+				<?php if ('customer-logout' === $endpoint): ?>
+					<li class="<?php echo wc_get_account_menu_item_classes($endpoint); ?> pl-2 pr-2 custom-logout">
+						<a class="pr-2 d-flex align-items-center text-danger"
+							href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>" <?php echo wc_is_current_account_menu_item($endpoint) ? 'aria-current="page"' : ''; ?>>
+							<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+								<path
+									d="M16.7363 9.03906V5.28906C16.7363 4.69233 16.4993 4.12003 16.0773 3.69807C15.6554 3.27612 15.0831 3.03906 14.4863 3.03906H8.48633C7.88959 3.03906 7.31729 3.27612 6.89534 3.69807C6.47338 4.12003 6.23633 4.69233 6.23633 5.28906V18.7891C6.23633 19.3858 6.47338 19.9581 6.89534 20.3801C7.31729 20.802 7.88959 21.0391 8.48633 21.0391H14.4863C15.0831 21.0391 15.6554 20.802 16.0773 20.3801C16.4993 19.9581 16.7363 19.3858 16.7363 18.7891V15.0391M12.9863 9.03906L9.98633 12.0391M9.98633 12.0391L12.9863 15.0391M9.98633 12.0391H22.7363"
+									stroke="#333333" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							</svg>
+							<span>Sign Out</span>
+						</a>
+					</li>
+				<?php else: ?>
+					<li class="<?php echo wc_get_account_menu_item_classes($endpoint); ?> pl-2 pr-2">
+						<a class="pr-2" href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>" <?php echo wc_is_current_account_menu_item($endpoint) ? 'aria-current="page"' : ''; ?>>
+							<?php echo esc_html($label); ?>
+						</a> |
+					</li>
+				<?php endif; ?>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 </div>
 
-<?php do_action( 'woocommerce_after_account_navigation' ); ?>
+<?php do_action('woocommerce_after_account_navigation'); ?>
