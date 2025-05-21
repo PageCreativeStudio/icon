0c624450fb73b9d90ac8ae0f491316654920ea78
PageCreativeStudio <web@pagecreative.co.uk>
2025-05-21 09:29:23 +0100

    Updated woocommerce

--- pagecreative/woocommerce/myaccount/navigation.php
+++ pagecreative/woocommerce/myaccount/navigation.php
@@ -24,7 +24,7 @@ do_action('woocommerce_before_account_navigation');
 
 <div class="container-fluid mx-auto max-100 px-2 px-md-4 mx-auto text-left pb-5 pt-3 pt-lg-4"
 	aria-label="<?php esc_html_e('Account pages', 'woocommerce'); ?>">
-	<div class="bordertop borderbottom font-18 font-mb-16 mb-lg-3 py-3">
+	<div class="bordertop borderbottom font-18 font-mb-16 mb-lg-3 pt-3 pb-1">
 		<ul class="accountnavigation d-grid d-md-flex flex-wrap">
 			<?php foreach (wc_get_account_menu_items() as $endpoint => $label): ?>
 				<?php if ('customer-logout' === $endpoint): ?>
@@ -40,7 +40,7 @@ do_action('woocommerce_before_account_navigation');
 						</a>
 					</li>
 				<?php else: ?>
-					<li class="<?php echo wc_get_account_menu_item_classes($endpoint); ?> pl-2 pr-2">
+					<li class="<?php echo wc_get_account_menu_item_classes($endpoint); ?> pl-2 pr-2 pb-2">
 						<a class="pr-2" href="<?php echo esc_url(wc_get_account_endpoint_url($endpoint)); ?>" <?php echo wc_is_current_account_menu_item($endpoint) ? 'aria-current="page"' : ''; ?>>
 							<?php echo esc_html($label); ?>
 						</a> |
